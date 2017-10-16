d2abdbd38dcfd6bff1e032f87e6d69023152121f
Nav Chaudhari <navish916@yahoo.com>
2017-10-16 12:28:27 -0700

    Namespaces for controllers
    Created a variable named namespaces to tell the routes which contoller to open since there is 2 controller with similar names

--- WebBlog/App_Start/RouteConfig.cs
+++ WebBlog/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using WebBlog.Controllers;
 
 namespace WebBlog
 {
@@ -13,11 +14,12 @@ namespace WebBlog
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            //defaults help secifiy controller and actions
 
-                                      //URL
-            routes.MapRoute( "Login", "login", new { controller = "Auth", action = "Login" });
-            routes.MapRoute( "Home", "", new { controller = "Posts", action = "index"});
+            // When you have identical controller names MVC doesn't know which one to open up. This way MVC can know which Controller to open up
+            var namespaces = new[] { typeof(PostsController).Namespace }; 
+
+            routes.MapRoute( "Login", "login", new { controller = "Auth", action = "Login" }, namespaces);
+            routes.MapRoute( "Home", "", new { controller = "Posts", action = "index"}, namespaces);
 
             //routes.MapRoute(
             //    name: "Default",
